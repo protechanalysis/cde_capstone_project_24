b2b929fb7b0fb2a2e81ea3fdb3a2481b6b0687a6
protechanalysis <adewunmioluwaseyi98@gmail.com>
2024-11-27 03:39:48 +0100

    isort

--- airflow/dags/capstone_airflow/create_table.py
+++ airflow/dags/capstone_airflow/create_table.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta
+
+from airflow import DAG
+
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 default_args = {'owner': 'adewunmi',
